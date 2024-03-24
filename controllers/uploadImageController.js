66277935d5b257689a46677ccec43e1d1eb002a4
Hein Swan Htet <hshscientist@gmail.com>
2024-03-24 13:03:23 +0630

    Added functionality to upload image

--- controllers/uploadImageController.js
+++ controllers/uploadImageController.js
@@ -1,5 +1,16 @@
+const { StatusCodes } = require('http-status-codes')
+const path = require('path')
+
 const uploadImage = async (req, res) => {
-    res.send('This is uploading image')
+    const catImage = req.files.image
+    const imagePath = path.join(
+        __dirname,
+        '../public/images/' + catImage.name
+    )
+
+    await catImage.mv(imagePath)
+
+    res.status(StatusCodes.OK).json({ image: { src: `/images/${catImage.name}` } })
 }
 
 module.exports = uploadImage
