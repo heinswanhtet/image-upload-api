a46dd80c651961ddbd50d081738ed112f231c592
Hein Swan Htet <hshscientist@gmail.com>
2024-03-24 13:30:39 +0630

    Connected to cloudinary

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ const rateLimiter = require('express-rate-limit')
 
 // file uploads
 const fileUpload = require('express-fileupload')
+const cloudinary = require('cloudinary').v2
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
+})
 
 // database
 const connectDB = require('./db/connection')
