871fde6ccc30dc833ef0c8733d42d61e02f92db7
Hein Swan Htet <hshscientist@gmail.com>
2024-03-24 13:19:45 +0630

    Added conditions and error handling to ensure the uploaded files to meet certain criteria

--- controllers/uploadImageController.js
+++ controllers/uploadImageController.js
@@ -1,8 +1,22 @@
-const { StatusCodes } = require('http-status-codes')
 const path = require('path')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError } = require('../errors')
 
 const uploadImage = async (req, res) => {
+    if (!req.files)
+        throw new BadRequestError('No file uploaded!')
+
     const catImage = req.files.image
+
+    if (!catImage.mimetype.startsWith('image'))
+        throw new BadRequestError('Please upload image file!')
+
+    const max_size = eval(process.env.IMAGE_MAX_SIZE)
+    // console.log(typeof (process.env.IMAGE_MAX_SIZE))
+    // console.log(max_size);
+    if (catImage.size > max_size)
+        throw new BadRequestError(`Please upload image smaller than ${max_size / (1024 * 1024)}MB`)
+
     const imagePath = path.join(
         __dirname,
         '../public/images/' + catImage.name
